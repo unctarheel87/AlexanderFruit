9a4804f0a89b267290435a096c52edb2549e2d52
John Tillman <john.w.tillman@gmail.com>
2020-07-06 16:10:50 -0400

    positioning

--- public/afcAccordion.js
+++ public/afcAccordion.js
@@ -9,10 +9,12 @@
     onOpen: function (wrapper, clickedHeader, content) {
       clickedHeader.classList.add('afc-accordion__header--open');
       clickedHeader.classList.remove('afc-accordion__header--close');
+      console.dir(content);
     },
     onClose: function (wrapper, clickedHeader, content) {
       clickedHeader.classList.remove('afc-accordion__header--open');
       clickedHeader.classList.add('afc-accordion__header--close');
+      content.classList.remove('afc-accordion__content--open');
     },
   });
   squeezebox.init();
